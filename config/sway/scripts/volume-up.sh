7116e73a4748da123fc8aa6e9f9d9ce1635c25f6
PineappleTrebuchet <niraveati23@gmail.com>
2025-12-17 15:09:57 -0500

    Misc. changes

--- config/sway/scripts/volume-up.sh
+++ config/sway/scripts/volume-up.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-current=$(pactl get-sink-volume @DEFAULT_SINK@ | head -n1 | grep -Po '\d+?(?=%)' | head -n1)
-if [ "$current" -lt 100 ]; then
-  pactl set-sink-volume @DEFAULT_SINK@ +5%
-fi
+#!/usr/bin/bash
 
+vol=$(pactl get-sink-volume @DEFAULT_SINK@ | head -n1 | awk -F'/' '{print $2}' | tr -d ' %')  # [web:10]
+
+if [ "$vol" -lt 100 ]; then
+    pactl set-sink-volume @DEFAULT_SINK@ +5%  # [web:6]
+fi
